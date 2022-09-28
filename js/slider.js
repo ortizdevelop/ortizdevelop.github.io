55dd364dbbcb93ede79035b53b7aef11e917c73f
ORTIZ <aidenortiz@mail.ru>
2022-09-28 23:05:59 +0300

    The main work on the "projects.pages.html" file is finished. It will be supplemented in the future, as new projects appear.
    html - additional id's have been added. Slightly changed tags and classes. Reworked some classes.
    
    scss/css - mainly the positioning of content on the page has been changed. Class "tools-and-links" got absolute positioning, so that it won't move when using slider.
    
    img - images have been added to the folder img/projects/slider to work with the file.
    
    js - additional variables were added to the function r2 and r3, icons and inscriptions in the "tools-and-links" class were changed when the slider was flipping. Text was added when changing slider, each slider has its own unique description, title and links.

--- js/slider.js
+++ js/slider.js
@@ -9,13 +9,20 @@ let projectsToolsAndLinks = document.querySelector("#projectsToolsAndLinks");
 let toolsTitle = document.querySelector("#toolsTitle");
 let linksTitle = document.querySelector("#linksTitle");
 
-let toolsImg = document.querySelector("#toolsImg");
+let toolsBody = document.querySelector("#toolsBody");
 let linksImg = document.querySelector("#linksImg");
+
+let toolsTitle1 = document.querySelector("#toolsTitle1");
+let toolsTitle2 = document.querySelector("#toolsTitle2");
+let toolsTitle3 = document.querySelector("#toolsTitle3");
+let toolsTitle4 = document.querySelector("#toolsTitle4");
 
 let toolsImg1 = document.querySelector("#toolsImg1");
 let toolsImg2 = document.querySelector("#toolsImg2");
 let toolsImg3 = document.querySelector("#toolsImg3");
 let toolsImg4 = document.querySelector("#toolsImg4");
+let toolsImg5 = document.querySelector("#toolsImg5");
+let toolsImg6 = document.querySelector("#toolsImg6");
 
 let linksText1 = document.querySelector("#linksText1");
 let linksText2 = document.querySelector("#linksText2");
@@ -45,13 +52,35 @@ r1.addEventListener('click', () => {
 
 r2.addEventListener('click', () => {
     projectsTitle.innerHTML = 
-    `<p><span>Origami</span> Caffe</p>`;
+    `<p>Caffe <span>Origami</span></p>`;
 
     projectsText.innerHTML = 
-    `<p>It's a fucking caffe, what do u want?</p>`;
+    `Start your day with aromatic coffee and a delicious dessert at - <a href="" target="_blank"><span>Caffe Origami</span></a>. <br>
+    Site of a coffee house in Pervouralsk, <br>
+    Is a fairly simple but modern design, <br>
+    designed by my friend <a href="https://vk.com/uxuiuliss" target="_blank"><span>"Uliss"</span></a>, <br>
+    The site looks very exciting and attractive. `;
+
+    toolsTitle2.textContent = 'React';
     
     toolsImg1.src = '/img/projects/slider/akar-icons_html-fill.png';
     toolsImg2.src = '/img/projects/slider/akar-icons_react-fill.png';
+    toolsImg3.src = '/img/projects/slider/akar-icons_css-fill.png';
+    toolsImg4.src = '/img/projects/slider/akar-icons_javascript-fill.png';
+});
+
+r3.addEventListener('click', () =>{
+    projectsTitle.innerHTML = "My <span>GitHub</span> Profile."
+
+    projectsText.innerHTML = 
+    `<p>A complete base and newsletter of all developments - <a href="https://github.com/AidenOrtiz" target="_blank"><span>ORTIZ DEVELOP GitHub</span></a>. <br> 
+    A rapidly growing profile since July 2022, actively updated with all new additions, projects and repositories, many of them, <br> 
+     in the public domain. The profile contains basic tools and links, as well as links to various social media.</p>`;
+
+    toolsTitle2.textContent = 'Python/Django';
+
+    toolsImg1.src = '/img/projects/slider/akar-icons_html-fill.png';
+    toolsImg2.src = '/img/projects/slider/akar-icons_python-fill.png';
     toolsImg3.src = '/img/projects/slider/akar-icons_css-fill.png';
     toolsImg4.src = '/img/projects/slider/akar-icons_javascript-fill.png';
 });

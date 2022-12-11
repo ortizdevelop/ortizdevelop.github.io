661834ef06b9ad926932a22ce5f27d52c0500318
ORTIZ <aidenortiz@mail.ru>
2022-12-11 21:24:38 +0300

    Progress on the "mobile-adaptation-content" branch (4).
    js - rewritten code for the "burgerMenu.js" script. Now the button works and when you click on it, it opens and closes.

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,15 +1,7 @@
-/**
- * When you click on the burger menu, the class 'activeBurgerMenu' is added to the element.
- */
-const burgerMenu = document.querySelectorAll('.header');
+/* Adding a class to the header element when the header element is clicked. */
+const burgerMenu = document.querySelector('.header');
 
-function DropDownMenuOn(){
-    burgerMenu.forEach((item) =>
-    item.classList.remove('activeBurgerMenu'));
-    this.classList.add('activeBurgerMenu');
-}
 
-// TODO: Finish the script, make a function that deletes the class when you click on it.
-
-burgerMenu.forEach((item) =>
-item.addEventListener('click', DropDownMenuOn));
+burgerMenu.addEventListener('click', function() {
+    burgerMenu.classList.toggle('activeBurgerMenu');
+})
